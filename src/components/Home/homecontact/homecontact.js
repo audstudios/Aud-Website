8a7ba659dbb3f4550e99400b80195e8d1bf01be0
audstudios <web@audstudios.com>
2025-10-12 20:18:31 -0400

    Line break in contact form and ice logo resize

--- src/components/Home/homecontact/homecontact.js
+++ src/components/Home/homecontact/homecontact.js
@@ -9,7 +9,7 @@ export default function HomeContact() {
                     <p>Contact Us</p>
                 </div>
                 <div className="homecontact-maincontent">
-                    <h3><b>Got a project or an aud job in mind?</b></h3>
+                    <h3><b>Got a project or an <br></br>aud job in mind?</b></h3>
                     <h3>We&apos;d love to connect</h3>
                 </div>
                 <div className="homecontact-button">
