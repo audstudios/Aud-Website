daee579e7ea27e0a783f2470f024d03d9f1e69bd
audstudios <web@audstudios.com>
2025-10-12 20:57:12 -0400

    Clean up on font sizes, removal of unneccsary hero block div, fixing services dropdown width

--- src/components/Home/homehero/homehero.js
+++ src/components/Home/homehero/homehero.js
@@ -14,7 +14,6 @@ export default function Homehero() {
       <h1 className="hero-title">we are not the standard.<b>we are aud studios.</b></h1>
       <img className="hero-logo" src="/images/AudGlassLogoV02.png"></img>
       <div className='hero-fade'></div>
-      <div className='hero-block'></div>
     </div>
   );
 }
