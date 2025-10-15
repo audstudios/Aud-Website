08d01bf47a470f16e2def94757d85aacb206c3a0
audstudios <web@audstudios.com>
2025-10-15 17:48:09 -0400

    Final Push horizontal project

--- src/app/prod/work/projects/templates/horizontal/page.js
+++ src/app/prod/work/projects/templates/horizontal/page.js
@@ -1,11 +1,13 @@
 import NavigationGeneral from "@/components/nav/navgeneral/navgeneral";
 import HorizontalProjectHero from "@/components/project/horizontal/projectpagehero/horizontalprojecthero";
+import HorizontalProjectContent from "@/components/project/horizontal/projectpagecontent/horizontalprojectcontent";
 
 export default function HorizontalProjectPage() {
   return (
     <>
       <NavigationGeneral />
       <HorizontalProjectHero />
+      <HorizontalProjectContent />
     </>
   );
 }
---
+++ src/components/project/horizontal/projectpagecontent/horizontalprojectcontent.js
@@ -0,0 +1,30 @@
+import '../../projecthero.css';
+import './horizontalprojectcontent.css';
+
+import Link from 'next/link'; 
+
+export default function HorizontalProjectContent() {
+  return (
+    <div className='horizontal-project-content-container'>
+      <div className='horizontal-project-content-wrapper'>
+        <div className='horizontal-project-left-content'>
+          <div className='horizontal-project-content-img-main'></div>
+          <div className='horizontal-project-content-img-main'></div>
+          <div className='horizontal-project-content-img-main'></div>                    
+        </div>
+        <div className='project-right-content'>
+          <h3 className='project-content-mainline'>Here is where you might put a sizzle line or something that kindof hooks the reader in , idk put something here like “Horse in a straw hat” or something engaging to really hook a user to read. Sometimes just a quote could be enough.</h3>
+          <p className='project-content-subcontent'>Here is where you can put some content giving background information, like what AUD did specifically, how you guys made the shoot happen. Tell or sell the story of what working with aud means in these sections.</p>
+          <p className='project-content-subcontent'>Here is where you can put some content giving background information, like what AUD did specifically, how you guys made the shoot happen. Tell or sell the story of what working with aud means in these sections.</p>
+          <p className='project-content-subcontent'>Here is where you can put some content giving background information, like what AUD did specifically, how you guys made the shoot happen. Tell or sell the story of what working with aud means in these sections.</p>
+          <p className='project-content-subcontent'>Here is where you can put some content giving background information, like what AUD did specifically, how you guys made the shoot happen. Tell or sell the story of what working with aud means in these sections.</p>
+          <div className='project-content-subimages-wrapper'>
+            <div className='project-content-horizontal-subimages'></div> 
+            <div className='project-content-horizontal-subimages'></div>
+            <div className='project-content-horizontal-subimages'></div>                   
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
