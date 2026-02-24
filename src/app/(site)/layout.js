7fa86dd8979cdda6c8dc67c97b192166ffc889f7
audstudios <billy.e.riley@gmail.com>
2026-02-24 03:42:57 -0500

    Scroll to top

--- src/app/(site)/layout.js
+++ src/app/(site)/layout.js
@@ -1,10 +1,12 @@
 import Header from '@/components/oldlanding/header/header';
 import Footer from '@/components/footer/footer';
 import MobileMenu from '@/components/nav/mobilenav/page';
+import ScrollToTop from '@/components/ScrollToTop';
 
 export default function SiteLayout({ children }) {
   return (
     <>
+      <ScrollToTop />
       <Header />
       <MobileMenu />
       <main>
