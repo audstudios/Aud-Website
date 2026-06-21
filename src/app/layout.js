3ce6b4e681520506dfa0a7a2a8cd3103dfeed57c
audstudios <billy.e.riley@gmail.com>
2026-06-21 19:09:04 -0400

    openGraph card

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,15 @@ import './globals.css';
 export const metadata = {
   title: 'Aud Studios',
   description: 'Aud Studios is a founder-led creative strategy and production agency, based in NYC.',
+  openGraph: {
+    title: 'Aud Studios',
+    description: 'Aud Studios is a founder-led creative strategy and production agency, based in NYC.',
+    siteName: 'Aud Studios',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+  },
 };
 
 export default function RootLayout({ children }) {
