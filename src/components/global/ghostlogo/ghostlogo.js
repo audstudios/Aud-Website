6e5c468874558f5bc8880047795489b80560c18d
audstudios <billy.e.riley@gmail.com>
2026-06-21 19:19:24 -0400

    Ghost logo fix

--- src/components/global/ghostlogo/ghostlogo.js
+++ src/components/global/ghostlogo/ghostlogo.js
@@ -1,11 +1,31 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
 import './ghostlogo.css';
 
 export default function GhostLogo() {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const footer = document.querySelector('.footer-container');
+      if (!footer || !ref.current) return;
+
+      const footerTop = footer.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+
+      ref.current.style.opacity = footerTop <= windowHeight ? '0' : '1';
+    };
+
+    window.addEventListener('scroll', handleScroll, { passive: true });
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className='ghostlogo'>
-      <img 
-        src="/images/global/aud_ghostlogo.svg" 
-        alt="AUD studios ghost logo" 
+    <div className='ghostlogo' ref={ref}>
+      <img
+        src="/images/global/aud_ghostlogo.svg"
+        alt="AUD studios ghost logo"
         role="presentation"
       />
     </div>
