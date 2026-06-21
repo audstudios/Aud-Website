0aa56bb975d27566eb8bc477cd17970e9b66c756
audstudios <billy.e.riley@gmail.com>
2026-06-21 19:04:24 -0400

    SEO fixes

--- src/app/(site)/contact/page.js
+++ src/app/(site)/contact/page.js
@@ -2,7 +2,7 @@ import ContactPageClient from './ContactPageClient';
 
 export const metadata = {
   title: 'Contact | Aud Studios',
-  description: 'Get in touch with Aud Studios. We'd love to connect about your next project.',
+  description: "Get in touch with Aud Studios. We'd love to connect about your next project.",
 };
 
 export default function ContactPage() {
